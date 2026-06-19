a154c143a6d96eec103af929979721939bfb43ea
luanrrsouza <luanrrdsouza@gmail.com>
2026-06-19 04:23:11 -0300

    chore(terraform): liberar ssh na ec2

--- terraform/security.tf
+++ terraform/security.tf
@@ -28,6 +28,13 @@ resource "aws_security_group" "ec2_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port       = 8080
     to_port         = 8080
     protocol        = "tcp"
